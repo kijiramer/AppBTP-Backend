f66e027775868643f019fab3c22ef6499b198e7c
kijiramer <39178594+kijiramer@users.noreply.github.com>
2025-09-29 18:00:07 +0200

    Increase JSON payload limit to 50MB for image uploads

--- App.js
+++ App.js
@@ -47,7 +47,8 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Connect to MongoDB
 connectDB();
